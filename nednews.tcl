1a88838ec7db4fe9a29dad4807813a26164d15a8
Edward Brekelbaum <nedbrek@yahoo.com>
2011-02-05 09:44:44 -0500

    Merged some lines

--- nednews.tcl
+++ nednews.tcl
@@ -5,18 +5,14 @@ toplevel .tMain
 ## a splitter for left and right windows
 pack [ttk::panedwindow .tMain.splitLR -orient horizontal] -expand 1 -fill both
 # a tree for news sources
-ttk::treeview .tMain.splitLR.tSrcs
-.tMain.splitLR add .tMain.splitLR.tSrcs
+.tMain.splitLR add [ttk::treeview .tMain.splitLR.tSrcs]
 
 ## a splitter for top and bottom (headers and bodies)
-ttk::panedwindow .tMain.splitLR.splitRTB -orient vertical
-.tMain.splitLR add .tMain.splitLR.splitRTB
+.tMain.splitLR add [ttk::panedwindow .tMain.splitLR.splitRTB -orient vertical]
 
 # a tree for headers (threaded view)
-ttk::treeview .tMain.splitLR.splitRTB.tHdr
-.tMain.splitLR.splitRTB add .tMain.splitLR.splitRTB.tHdr
+.tMain.splitLR.splitRTB add [ttk::treeview .tMain.splitLR.splitRTB.tHdr]
 
 # textbox for bodies
-text .tMain.splitLR.splitRTB.xBdy -state disabled
-.tMain.splitLR.splitRTB add .tMain.splitLR.splitRTB.xBdy
+.tMain.splitLR.splitRTB add [text .tMain.splitLR.splitRTB.xBdy -state disabled]
 
